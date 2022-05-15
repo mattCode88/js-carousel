8127319efcf091c31d8b9d063efdcc6dbfa8d598
mattcode88 <mattcode88@gmail.com>
2022-05-15 10:23:49 +0200

    animazioni aggiunte

--- script.js
+++ script.js
@@ -2,20 +2,22 @@ export default class Carousel extends HTMLElement{
     constructor() {
         super();
 
-        this.carouselContainer = document.getElementById('carousel-p');
         this.imagesCarousel = document.getElementsByClassName('images-carousel');
-        this.prev = document.getElementById('prev-p');
-        this.next = document.getElementById('next-p');
+        this.prev = document.getElementById('prev');
+        this.next = document.getElementById('next');
 
         this.image = [
-            'https://cdn.pixabay.com/photo/2018/05/17/09/18/away-3408119__480.jpg',
-            'https://www.artemedialab.it/wp-content/uploads/2019/04/immagini-sfondo-1-700x400.jpg',
-            'https://www.servizi-web-marketing.it/images/stories/news/immaginedascaricare.jpg',
-            'https://www.addlance.com/blog/wp-content/uploads/2019/04/immagini-da-scaricare.jpg',
-            'https://i0.wp.com/www.giacomocusano.com/wp-content/uploads/2016/07/coastal-wash-web.jpg?fit=1024%2C682&ssl=1',
-            'https://www.donnamoderna.com/content/uploads/2020/11/tramonto-480x360.jpg',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROrunHGF1h2VaInnlgYRJmYDiXjkTYLtDc_A&usqp=CAU',
-            'https://vitolavecchia.altervista.org/wp-content/uploads/2017/04/Formati-e-qualit%C3%A0-di-unimmagine-digitale.jpg'
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336351017.jpg?k=009c57740b67e4fc8e9ae472f988d55b26f2a872832f17d6d45c510d7b7af7c2&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350897.jpg?k=67724238a8167f75d7c3a500afb3cd3dc7104af70bd83796e6f66ad983ff810c&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350957.jpg?k=2e099d4143993bc133562417098a9efb8ac1403a20bca06dd6a97a6eafff8d6c&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350881.jpg?k=46bd8f989c6f17875cdeeb41f2cc4cbabb0dab66e4ec1ea40890a62a94c7c6f5&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350980.jpg?k=7b5573fe0e2e43a629f6f8fba2da1bc8d3e0cb6a27f46629a4f7e121ee212182&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350975.jpg?k=eb7745ff8daf7062eb3aaa996425842578018a415bb9e4daf0d85970ed8ad815&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350999.jpg?k=fb6b1bf1e8be466c923ac9f75828df231353b8db391e0d9444e1fddb6ab9a2cd&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350859.jpg?k=303b028054f8af1b4d68d10698b6b309acdfcd1e96a9b576dec230c805479a74&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336351023.jpg?k=157efd53b3a9845f4ebd792ff0bfc359780ea71da8356ae0d12fb5e0e304c0f3&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336350771.jpg?k=e1103f9de93fd04499be55dbdf78e432f1b87856e8af1d294bbd97598a8953a3&o=&hp=1',
+            'https://cf.bstatic.com/xdata/images/hotel/max1024x768/336351046.jpg?k=d851e1328f043ad5a044ae48d50b1b4cc416dc27f6d4a67a64411b596a996719&o=&hp=1'
         ];
 
         this.indexImage = 1;
@@ -37,6 +39,10 @@ export default class Carousel extends HTMLElement{
     nextImage = () => {
         this.next.addEventListener('click', e => {
 
+            gsap.fromTo('.image2', { opacity: 0, scale: 0, rotation: -360 }, { duration: .5, opacity: 1, scale: 1, rotation: 0 });
+            gsap.fromTo('.image1', { opacity: 0, scale: 0, rotation: -720 }, { duration: 1, opacity: 1, scale: 1, rotation: 0 });
+            gsap.fromTo('.image3', { opacity: 0, scale: 0, rotation: -720 }, { duration: 1, opacity: 1, scale: 1, rotation: 0 });
+
             this.indexImage++;
 
             if (this.indexImage > this.image.length - 1) this.indexImage = 0;
@@ -49,6 +55,10 @@ export default class Carousel extends HTMLElement{
     prevImage = () => {
         this.prev.addEventListener('click', e => {
 
+            gsap.fromTo('.image2', { opacity: 0, scale: 0, rotation: 360 }, { duration: .5, opacity: 1, scale: 1, rotation: 0 });
+            gsap.fromTo('.image1', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, opacity: 1, scale: 1, rotation: 0 });
+            gsap.fromTo('.image3', { opacity: 0, scale: 0, rotation: 720 }, { duration: 1, opacity: 1, scale: 1, rotation: 0 });
+
             this.indexImage--;
 
             if (this.indexImage < 0) this.indexImage = this.image.length - 1;
@@ -59,20 +69,3 @@ export default class Carousel extends HTMLElement{
     };
     
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
